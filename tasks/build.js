b1aa8ef822e9f931c446fd1c387f4f015f86aa50
Ryan Dy <rdy@pivotal.io>
2015-08-05 15:09:37 -0700

    update build for event source, use loose mode in babel for real
    Signed-off-by: Tyler Phelan <tphelan@pivotal.io>

--- tasks/build.js
+++ tasks/build.js
@@ -2,12 +2,29 @@ var del = require('del');
 var gulp = require('gulp');
 var mergeStream = require('merge-stream');
 var plugins = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
 
-gulp.task('clean', callback => del('dist/*', callback));
+const COPYRIGHT = '//(c) Copyright 2015 Pivotal Software, Inc. All Rights Reserved.\n';
 
-gulp.task('build', ['clean'], function() {
+gulp.task('clean', function(callback) {
+  del('dist', callback);
+});
+
+gulp.task('build', function(callback) {
+  runSequence('clean', 'babel', callback);
+});
+
+gulp.task('babel', function() {
   return mergeStream(
-    gulp.src(['src/**/*.js', 'mocks/**/*.js']).pipe(plugins.babel()),
+    gulp.src(['src/**/*.js', 'mocks/**/*.js']).pipe(plugins.babel()).pipe(plugins.header(COPYRIGHT)),
     gulp.src(['LICENSE', 'README.md', 'package.json'])
   ).pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', function(callback) {
+  runSequence('clean', 'babel', callback);
+});
+
+gulp.task('watch', ['build'], function() {
+  gulp.watch(['src/**/*.js', 'mocks/**/*.js'], ['babel']);
 });
